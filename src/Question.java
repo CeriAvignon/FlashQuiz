ed1553da509a4c1ed378e96e3b024e5b62797c8e
Faellyss <Faellyss@users.noreply.github.com>
2016-11-02 17:21:39 +0100

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+import java.util.Vector;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Question
+{
+	private int ID_Question;
+	protected String Intitule;
+	protected int Type_Question;
+	protected String Media_File;
+	protected int Type_Media;
+	protected int ID_User;
+	protected int timer;
+	Connection connection;
+	
+	
+	public bool CreateQuest()
+	{
+		// Requete SQL de création dans la table
+	}
+	public bool DeleteQuest()
+	{
+		
+	}
+	public bool ModifyQuest()
+	{
+		
+	}
+}
