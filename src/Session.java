c14f1d520138e38b6e170c0d97d83d7317f32ea8
Faellyss <Faellyss@users.noreply.github.com>
2016-11-02 17:22:11 +0100

    Update Session.java

--- src/Session.java
+++ src/Session.java
@@ -1,1 +1,19 @@
-
+class Session
+{
+	//id
+	//id auteur
+	//vecteur serie
+	//vecteur question
+	//date debut
+	//date fin
+	
+	public bool CreatSes()
+	{
+	}
+	public bool SuprSes()
+	{
+	}
+	public bool ModifSes()
+	{
+	}
+}
