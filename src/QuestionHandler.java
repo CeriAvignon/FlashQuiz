f90dfd06b18833dc7b01a0861170af974718f394
Benjamin Rock <benj_84230@hotmail.fr>
2016-11-22 17:01:59 +0100

    Update QuestionHandler.java

--- src/QuestionHandler.java
+++ src/QuestionHandler.java
@@ -1,4 +1,3 @@
-package ques;
 import ConnexionJM.java;
 import java.sql.*;
 
@@ -6,30 +5,33 @@ public class QuestionHandler {
 	
 	public void sendQuestion(Question question)
 	{
+		/****************Récupération de la question ***********************/
 		int idQuestion = question.getIdQuestion();
 		String contenu = question.getIntitule();
 		int typeQuestion = question.getTypeQuestion();
 		String mediafile = question.getMediafile();
 		int typeMedia = question.getTypeMedia();
+		/*******************************************************************/
 		
 		Statement statement = connexion.createStatement();
 		int retour;
 		ResultSet res=null;
-		if(idQuestion == -1)
+		if(idQuestion == -1) //La question n'existe pas
 		{
 			res = statement.executeQuery( "SELECT * FROM Media WHERE Contenu_media=mediafile;" );//COUNT(*)
-			if(res == null)
+			if(res == null)//Le media n'existe pas
 			{
 				retour = statement.executeUpdate("INSERT INTO Media(TYPE,Contenu_media) VALUES(typeMedia,mediafile)");
 			}
 			res = statement.executeQuery( "SELECT ID_Media FROM Media;" );
-			retour = statement.executeUpdate("INSERT INTO Questions(TYPE_Question,Contenu_Question,"+ "ID_Media) VALUES(contenu,typeQuestion,)");
+			res=resultat.next();
+			retour = statement.executeUpdate("INSERT INTO Questions(TYPE_Question,Contenu_Question,ID_Media) VALUES(contenu,typeQuestion,idMedia)");
 		}
-		else
+		else //La question existe déjà
 		{
-			
+			//faire un update
 		}
 		
-		
+		//faire exception pour retour d'id
 	}
 };
