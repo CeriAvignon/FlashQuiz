f2d8d2835a02e1506b8c6eb047c444c4c741bdbb
Le Veve Mathieu <levevemathieu@gmail.com>
2016-11-22 09:39:28 +0100

    Update Session.java

--- src/Session.java
+++ src/Session.java
@@ -1,19 +1,17 @@
+
+/*! 
+*	\file Session.java
+*	\brief Fichier java Session
+*	\author {Le Veve Mathieu}
+*	\date xx/10/2016
+*	\date 22/11/2016
+*
+*	\class <Session> Session.java 	Session
+*	\brief	Fichier en java qui crée une session, pour le moment avec ses getters/ setters pour créer une session
+*	pour le moment sans intéraction avec la BDD. 
+*/
+
 class Session
 {
-	//id
-	//id auteur
-	//vecteur serie
-	//vecteur question
-	//date debut
-	//date fin
-	
-	public bool CreatSes()
-	{
-	}
-	public bool SuprSes()
-	{
-	}
-	public bool ModifSes()
-	{
-	}
+
 }
