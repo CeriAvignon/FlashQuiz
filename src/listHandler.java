460fbd0a8a5bb141674dd072dcd6b84428a74d80
Renaud Galy <renodu84@hotmail.fr>
2016-11-22 15:38:07 +0100

    Create listHandler.java

---
+++ src/listHandler.java
@@ -0,0 +1,17 @@
+/*import ConnexionJM.java;*/
+/*import Liste.java;*/
+import java.util.Vector;
+
+public class listHandle {
+	private String nomListe;
+	private int idAuthor;
+	private int idListe;
+	protected Vector<Question>listeQuestion;
+	
+	public void sendList(Liste list)
+	{
+		
+		
+	}
+	
+}
