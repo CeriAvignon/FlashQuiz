eebde954d09352cbd86084fb3847b1d5649b0587
Benoit Dorey <benoitdorey@gmail.com>
2016-11-22 14:37:41 +0100

    Add files via upload

--- src/stat_session.java
+++ src/stat_session.java
@@ -1,7 +1,31 @@
+/**
+ * Stat_session
+ *
+ * @author Benoît Dorey
+ */
 public class stat_session
 {
 
+	/**
+	 * Function for multiple choice
+	 *
+	 * @param prop
+	 *            String array that contain all the propositions for the question
+	 * @param res_true
+	 *            String array that contain all the right answers for this question, like ['A','A','B',...] 
+	 * @param res_false
+	 *            String array that contain all the wrong answers for this question, like ['C','D','D',...] 
+	 * @return 
+	 *		Double array that contain all the statistics values of the question, like A% = Res[0];
+	 */
+
 	// Fonction pour choix multiples
+	//
+	//	Sert à sortir des statistiques plus précise pour une question de type choix multiples, cependant, si cette question se trouve
+	//	dans une session de plusieurs questions de différents type alors on préférera la deuxième fonction qui sort le pourcentage de 
+	//	réponses vraies et fausses pour avoir des stats sur la véracité ou non des réponses. Bonne réponse à la session = X% et mauvaise 
+	// 	réponses à la session = Y%
+	//
 	public static double[] stat_mult(String[] prop, String[] res_true, String[] res_false)
 	{
 		// Total de réponses
@@ -10,6 +34,24 @@ public class stat_session
 		int nbr_prop = prop.length;
 		// Tableau contenant les statistiques demandées
 		double Res[] = new double[nbr_prop];
+/*
+		// Tableau qui contient "true" ou "false" en fonction des prop
+		boolean T_F[] = new boolean[nbr_prop];
+
+		for (int i=0;i<nbr_prop;i++)
+		{
+			for(int j=0;j<res_true.length;j++)
+			{
+				if (prop[i] == res_true[j])
+				{
+					T_F[i] = true;
+					break;
+				r}
+				else T_F[i] = false;
+				break;
+			}
+		}
+*/
 
 		// Tableau qui contient la population pour chaque variable proposition
 		double NBR[] = new double[nbr_prop];
@@ -48,6 +90,18 @@ public class stat_session
 		return Res;
 	}
 
+
+	/**
+	 * Function who calculate statistic of the session (all type of question together)
+	 *
+	 * @param res_true
+	 *            Int array that contain the number of good answer for this session
+	 * @param res_false
+	 *            Int array that contain the number of bad answer for this session
+	 * @return 
+	 *		Double array that contain statistics values for good and bad answers, like: Res[0] = 45 and Res[1] = 55; so 45% true 
+	 *		and 55% false
+	 */
 	// Fonction pour les tests autres que choix multiples
 	public static double[] stat_result(int res_true, int res_false)
 	{
@@ -67,7 +121,9 @@ public class stat_session
 
 
 
-
+	/**
+	 * Main, to test function
+	 */
 	// Main de test des deux fonctions et de présentation des entrées
 	//
 	// stat_result demande le nombre de bonne réponses et de mauvaises
