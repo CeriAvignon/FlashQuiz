59988101427e255b5e424909c1bfc9b450f22472
Renaud Galy <renodu84@hotmail.fr>
2016-11-22 16:45:25 +0100

    Update listHandler.java

--- src/listHandler.java
+++ src/listHandler.java
@@ -1,6 +1,9 @@
+package projetProg;
 /*import ConnexionJM.java;*/
 /*import Liste.java;*/
+import java.sql.*;
 import java.util.Vector;
+
 
 public class listHandle {
 	private String nomListe;
@@ -10,6 +13,33 @@ public class listHandle {
 	
 	public void sendList(Liste list)
 	{
+		idListe = list.getIdListe();								//
+		idAuthor = list.getIdAuthor();
+		nomListe = list.getNomListe();
+		listeQuestion = list.getListeQuestion();
+		int statut;
+		Statement statement = connexion.createStatement();
+		
+		if(idListe == -1)
+		{
+			ResultSet res = statement.executeQuery("SELECT FROM Liste_Metadata WHERE Titre_Liste = idListe;");
+			if(res == null)
+			{
+		
+				statut = statement.executeUpdate("INSERT INTO Liste_Metadata(ID_Utilisateur, Titre_Liste) VALUES(idAuthor, nomListe);");
+		
+			}
+			
+			else
+			{
+				// Retourner une erreur de type "titre déjà existant"
+			}
+		}
+		
+		else
+		{
+			
+		}
 		
 		
 	}
