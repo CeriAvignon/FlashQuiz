7728e7aa3d1ea84a83760af3002a0f7f05034702
Alexandra MOSHINA <alexamoshina@mail.ru>
2016-11-22 16:28:12 +0100

    Add files via upload

---
+++ src/SessionWeb.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import ConnexionJM.java;
+
+public class SessionWeb {
+
+private int idSession;
+private int idListe;
+private String dateOpen;
+private String dateClose;
+private boolean typeSession;
+private String nameSession;
+
+public void sendSession(Session s)
+	{
+	idSession = s.getIdSession();
+	idListe = s.getIdListe();
+	dateOpen = s.getDateOpen();
+	dateClose = s.getDateClose();
+	typeSession = s.getTypeSession();
+	nameSession = s.getNameSession();	
+		
+
+
+
+
+	}
+}
