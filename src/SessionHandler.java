7cc4b5db0b943b73989d2e0278e4576691d938aa
Alexandra MOSHINA <alexamoshina@mail.ru>
2016-11-22 17:12:34 +0100

    Update SessionHandler.java

--- src/SessionHandler.java
+++ src/SessionHandler.java
@@ -23,8 +23,31 @@ public void sendSession(Session s)
 	int ret;	
 	Statement statement = connexion.createStatement();
 	ResultSet res=null;	
-
-
+		if(idSession == -1) // si session n'existe pas
+	{
+		res = statement.executeQuery( "SELECT * FROM Sessions WHERE Titre_Session=nameSession;");
+		if(res == null)
+		{
+			ret.statement.executeUpdate("INSERT INTO Sessions(ID_Session, ID_Liste, Date_Ouverture, Date_Fermeture, Type_Session, Titre_Session) VALUES(idSession, idListe, dateOpen, dateClose, typeSession, nameSession);");
+		}
+			
+		else
+		{
+			 System.out.println("ERREUR! Session existe deja!");
+		}
+	}
+	else //si session existe
+	{
+		res = statement.executeQuery( "SELECT * FROM Sessions WHERE Titre_Session=nameSession;");
+	}
+		if(res == null)
+		{
+		System.out.println("ERREUR! Session n'existe pas!");
+		}
+		else
+		{
+		//update
+		}
 
 
 	}
