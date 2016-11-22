e399d942c449377a78a3a44e3d5976562d993a21
Renaud Galy <renodu84@hotmail.fr>
2016-11-22 17:20:45 +0100

    Update ListHandler.java

--- src/ListHandler.java
+++ src/ListHandler.java
@@ -12,22 +12,22 @@ public class listHandle {
 	
 	public void sendList(Liste list)
 	{
-		idListe = list.getIdListe();															// Récupère l'id de la liste
-		idAuthor = list.getIdAuthor();															// Récupère l'id du créateur de la liste
-		nomListe = list.getNomListe();															// Récupère le nom de la liste
-		listeQuestion = list.getListeQuestion();												// Récupère les questions dans le vecteur listeQuestion de l'objet liste
+		idListe = list.getIdListe();									// Récupère l'id de la liste
+		idAuthor = list.getIdAuthor();									// Récupère l'id du créateur de la liste
+		nomListe = list.getNomListe();									// Récupère le nom de la liste
+		listeQuestion = list.getListeQuestion();							// Récupère les questions dans le vecteur listeQuestion de l'objet liste
 		int statut;
-		Statement statement = connexion.createStatement();										// Création d'une requête
+		Statement statement = connexion.createStatement();						// Création d'une requête
 		
-		if(idListe == -1)																		// S'il s'agit d'une création de liste
+		if(idListe == -1)										// S'il s'agit d'une création de liste
 		{
 			ResultSet res = statement.executeQuery("SELECT FROM Liste_Metadata "
-													+ "WHERE Titre_Liste = idListe;");			// Vérifie si le titre de liste est déjà présent dans la BDD
+								+ "WHERE Titre_Liste = idListe;");		// Vérifie si le titre de liste est déjà présent dans la BDD
 			if(res == null)
 			{
 		
 				statut = statement.executeUpdate("INSERT INTO Liste_Metadata(ID_Utilisateur,"
-												+ " Titre_Liste) VALUES(idAuthor, nomListe);");	// Insert l'id utilisateur et le titre de la liste dans la BDD
+								+ " Titre_Liste) VALUES(idAuthor, nomListe);");	// Insert l'id utilisateur et le titre de la liste dans la BDD
 		
 			}
 			
@@ -37,7 +37,7 @@ public class listHandle {
 			}
 		}
 		
-		else																				// S'il s'agit d'une modification de liste
+		else												// S'il s'agit d'une modification de liste
 		{
 			
 		}
