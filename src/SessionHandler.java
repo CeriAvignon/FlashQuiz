4b5435f4b58c22836a9ba7442f86b40ac3c97c49
Alexandra MOSHINA <alexamoshina@mail.ru>
2016-11-24 11:01:27 +0100

    Update SessionHandler.java

--- src/SessionHandler.java
+++ src/SessionHandler.java
@@ -16,35 +16,62 @@ protected Vector<Liste>listeliste;
 public void sendSession(Session s)
 	{
 	idsession = s.getIdSession();						//recupere l'id de la session
-	datedebut = s.getDateDebut();						// recupere la date de debut
-	datefin = s.getDateFin();						// recupere la date de debut
+	datedebut = s.getDateDebut();						//recupere la date de debut
+	datefin = s.getDateFin();						//recupere la date de debut
 	typesession = s.getTypeSession();					//recupere le type de session
-	namesession = s.getNomSession();					// recupere le nom de session
+	namesession = s.getNomSession();					//recupere le nom de session
 	password = s.getPassword();						//recupere le mot de passe
-	listeliste = list.getListeListe(); 					//recupere la liste des listes
+	listeliste = s.getListeListe(); 					//recupere la liste des listes
 	
-	int ret;
-	int req; //cree une requette
-	Statement statement = connexion.createStatement();
+	String query="";
+	Statement statement = cnx.createStatement();
 	ResultSet res=null;	
-	try{
-		if(idSession == -1) // si session existe pas
+	PreparedStatement prepare = cnx.prepareStatement();	
+	try
 	{
+		if(idSession == -1) // si session existe pas
+		{
 			//on insere les donnees dans la BDD
-			ret.statement.executeUpdate("INSERT INTO Sessions(Date_Ouverture, Date_Fermeture, Type_Session, Titre_Session) VALUES($datedebut, $datefin, $typesession, $namesession);");
+			query = "INSERT INTO Sessions_Metadata(Date_Ouverture, Date_Fermeture, Type_Session, Titre_Session, MotDePasse) VALUES(?, ?, ?, ?, ?);";
+					prepare =  cnx.prepareStatement(query);
+					prepare.setObject(1,datedebut);
+					prepare.setObject(2,datefin);
+					prepare.setObject(3,typesession);
+					prepare.setObject(4,namesession);
+					prepare.setObject(5,password);
+					prepare.executeUpdate();
+					prepare.close();
 			//on recupere l'id de la session
-			res.statement.executeQuery("SELECT currval(pg_get_serial_sequence('Sessions','ID_Session'));");
-			for (List l:listeliste) {
+			res = statement.executeQuery("SELECT currval(pg_get_serial_sequence('Sessions','ID_Session')) as id;");
+			s.setIdSession(res.getInt(1));
+			for (List l:listeliste) 
+			{
+				//envoye la liste
 				sendListe(l);
-				if (l.getIDLIste() != -1)  {
+				//si la liste s'est bien enregistrée
+				if (l.getIdListe() != -1)  
+				{
 					// Ajouter dans Session_Contenu 
-					req.statement.executeUpdate("INSERT INTO Session_Contenu (ID_Session,ID_Liste) values (?,?));");
+					query = "INSERT INTO Session_Contenu (ID_Session,ID_Liste) values (?,?));";
+					prepare =  cnx.prepareStatement(query);
+					prepare.setObject(1,s.getIdSession());
+					prepare.setObject(2,s.getIdListe());
+					prepare.executeUpdate();
+					prepare.close();
+					res.close();
+				}
+				else
+				{
+					System.out.println("ERREUR!");
 				}
 			}
-	}
-	else //si session existe
+		}
+		else //si session existe
+			
+			
+			
 	{
-		res = statement.executeQuery( "SELECT * FROM Sessions WHERE ID_Session=$idsession;");
+		res = statement.executeQuery( "SELECT * FROM Sessions WHERE ID_Session=idsession;");
 	}
 		if(res == null)
 		{
@@ -53,7 +80,7 @@ public void sendSession(Session s)
 		else
 		{
 		//on fait la mise a jour de la BDD
-		ret.statement.executeUpdate("UPDATE Sessions SET ;");
+		ret = statement.executeUpdate("UPDATE Sessions SET ;");
 			// Supprimer tout dans Session_Contenu WHERE ID_Session = $idsession
 			for (List l:listeliste) {
 				sendListe(l);
