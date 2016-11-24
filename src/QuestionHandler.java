5b1226501a3631a2424749c6f5cd7b670e5707b5
Benjamin Rock <benj_84230@hotmail.fr>
2016-11-24 09:50:27 +0100

    Update QuestionHandler.java

--- src/QuestionHandler.java
+++ src/QuestionHandler.java
@@ -1,3 +1,4 @@
+package ques;
 import ConnexionJM.java;
 import java.sql.*;
 
@@ -13,16 +14,40 @@ public class QuestionHandler {
 		int typeMedia = question.getTypeMedia();
 		/*******************************************************************/
 		
-		Statement statement = connexion.createStatement();
-		int retour;
-		ResultSet res=null;
+		String query="";
+		//PreparedStatement prepare = conn.prepareStatement(query);
+		//Statement statement = connexion.createStatement();
+		//int retour;
+		PreparedStatement prepare;
+		Statement statement = cnx.createStatement();
+		ResultSet res;
+		int id_media;
 		if(idQuestion == -1) //La question n'existe pas
 		{
-			res = statement.executeQuery( "SELECT * FROM Media WHERE Contenu_media=mediafile;" );//COUNT(*)
-			if(res == null)//Le media n'existe pas
+			/*************************Requete qui verifie l'existence du media*********************/
+			query="SELECT * FROM Media WHERE Contenu_media=?;";
+			prepare = cnx.prepareStatement(query);
+			prepare.setObject(2,mediafile); 
+			res = prepare.executeQuery();
+			boolean retour=res.first();
+			prepare.close();
+			res.close();
+			/*************************************************************************************/
+			if(!retour)//Le media n'existe pas
 			{
-				retour = statement.executeUpdate("INSERT INTO Media(TYPE,Contenu_media) VALUES(typeMedia,mediafile)");
+				
+			/**********************Ajout du media si il n'existe pas******************************/
+				query="INSERT INTO Media(TYPE,Contenu_media) VALUES(?,?);";
+				prepare.setObject(1,typeMedia);
+				prepare.setObject(2,mediafile);
+				prepare.executeUpdate();
+				prepare.close();
+				res.close();
+				res.statement.executeQuery("SELECT curval(pg_get_serial_sequence('Media','ID_Media'));");
+				id_media = res.getInt("ID_Media");
+			/*************************************************************************************/
 			}
+			
 			res = statement.executeQuery( "SELECT ID_Media FROM Media;" );
 			res=resultat.next();
 			retour = statement.executeUpdate("INSERT INTO Questions(TYPE_Question,Contenu_Question,ID_Media) VALUES(contenu,typeQuestion,idMedia)");
