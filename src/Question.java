eb2721e1422555f1d67c9030d6c122a05643436b
Le Veve Mathieu <levevemathieu@gmail.com>
2016-11-27 13:10:09 +0100

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -6,20 +6,28 @@ import java.sql.DriverManager;
 
 public class Question
 {
-	private int idquestion;
+	protected int idquestion;
 	protected String intitule;
 	protected int typequestion;
 	protected String mediafile;
 	protected int typemedia;
-	protected int idautho;
+	protected int idauteur;
 	protected int timer;
 	//Connection connection;
+	
+	/*public Question()
+	{
+		
+		
+	}
 	
 	
 	public boolean createQuest()
 	{
 		// Requete SQL de création dans la table
 	}
+	
+	
 	/*public boolean celeteQuest()
 	{
 		
