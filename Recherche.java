3a7702e4d5c7e684223847ed250309d58c2f9082
ChambonBrice <Brice.chambon@outlook.com>
2016-11-30 23:16:13 +0100

    Add files via upload

---
+++ Recherche.java
@@ -0,0 +1,18 @@
+package ig.flashquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class Recherche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche);
+    }
+
+}
