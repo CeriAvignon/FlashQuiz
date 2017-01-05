65dd290d7b5a1d9ab1db380f847c2b71c0fdbb6b
Dimitri HUEBER <dimidu13190@hotmail.fr>
2017-01-05 01:05:07 +0100

    premier jet
    a venir:
    creer les fonctions java utile au fonctionnement

---
+++ CreationQuizz(Android).java
@@ -0,0 +1,13 @@
+package ceri.createquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Createquizz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createquizz);
+    }
+}
