10efec92c23bcfec5fb313e5bde15d4d5a0c9ca4
Alexandra MOSHINA <alexamoshina@mail.ru>
2017-01-10 17:46:27 +0100

    Update SessionHandler.java

--- src/SessionHandler.java
+++ src/SessionHandler.java
@@ -132,7 +132,7 @@ public void sendSession(Session s) throws SQLException
 			s.setIdSession(res.getInt(1));
 			
 			
-			for (Liste l:listeliste) // pour chaque element du Vecteur Liste
+			for (Liste l:listeliste) // pour chaque element du Vecteur
 			{
 				//envoye la liste
 				//sendListe(l);
