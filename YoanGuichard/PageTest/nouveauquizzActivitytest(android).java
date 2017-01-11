a46daeefce577d256efcba7ca6e68232ac8fbf15
Olfar <plow006@gmail.com>
2017-01-11 17:54:53 +0100

    Update nouveauquizzActivitytest(android).java

--- YoanGuichard/PageTest/nouveauquizzActivitytest(android).java
+++ YoanGuichard/PageTest/nouveauquizzActivitytest(android).java
@@ -3,11 +3,11 @@ package com.example.yoan.flashquizz;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class participQuizzActivity extends Activity {
+public class nouveauquizzActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.participquizz);
+        setContentView(R.layout.nouveauquizz);
     }
 }
