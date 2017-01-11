c7ff44be1476507eead718c070b288d515c9f906
Olfar <plow006@gmail.com>
2017-01-11 17:51:45 +0100

    Create stat2Activitytest(android).java

---
+++ YoanGuichard/PageTest/stat2Activitytest(android).java
@@ -0,0 +1,13 @@
+package com.example.yoan.flashquizz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class stat2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stat2);
+    }
+}
