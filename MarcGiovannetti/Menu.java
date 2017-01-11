23c789a4a4c219adc7aedf356a84ee3d801e431a
Marc Giovannetti <marc.giovannetti@gmail.com>
2017-01-11 18:16:47 +0100

    Update Menu.java

--- MarcGiovannetti/Menu.java
+++ MarcGiovannetti/Menu.java
@@ -1,4 +1,5 @@
-package Login;
+package Menu;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -10,6 +11,8 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+
+import Login.Login;
 
 import javax.swing.BoxLayout;
 import javax.swing.JOptionPane;
@@ -35,6 +38,14 @@ public class Menu extends JFrame{
 		this.setLocationRelativeTo(null);
 		//this.setResizable(false);
 		
+		//Action boutons
+		m_butCreat.addActionListener(new BoutonCreat());
+		m_butRep.addActionListener(new BoutonRep());
+		m_butStat.addActionListener(new BoutonStat());
+		m_butParam.addActionListener(new BoutonParam());
+		m_butDec.addActionListener(new BoutonDec());
+		m_butQuit.addActionListener(new BoutonQuit());
+		
 		//Panneau de tête (logo et autre info)
 		JPanel m_panHead = new JPanel();
 		m_panHead.setMaximumSize( new Dimension(300, 100) );
@@ -43,8 +54,6 @@ public class Menu extends JFrame{
 		//Panneau principal
 		JPanel m_panMainMenu = new JPanel();
 		this.setLayout(new GridLayout(8, 1, 10, 10));
-		//m_butLog.addActionListener(new BoutonLog());
-		//m_butQuitter.addActionListener(new BoutonQuit());
 		m_panMainMenu.setLayout(new BoxLayout(m_panMainMenu, BoxLayout.LINE_AXIS));
 		this.getContentPane().add(m_butCreat);
 		this.getContentPane().add(m_butRep);
@@ -56,5 +65,45 @@ public class Menu extends JFrame{
 		this.getContentPane().add(m_panMainMenu);
 		this.setVisible(true);
 	}
-
+	
+		
+	//Action des bouttons
+	
+	class BoutonCreat implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Direction : Créer nouveau Quizz");
+		}
+	}
+	
+	class BoutonRep implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Direction : Répondre à un Quizz");
+		}
+	}
+	
+	class BoutonStat implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Direction : Afficher stat");
+		}
+	}
+	
+	class BoutonParam implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Direction : Afficher paramètre");
+		}
+	}
+	
+	class BoutonDec implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			Login m_login = new Login();
+			dispose(); 
+		}
+	}
+	
+	class BoutonQuit implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+	
 }
