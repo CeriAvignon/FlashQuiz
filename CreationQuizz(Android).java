e34b19d66b594270716ab24ac8715ea1026f6a92
Dimitri HUEBER <dimidu13190@hotmail.fr>
2017-01-11 15:25:15 +0100

    ajout de transition pour modif_questionnaire

--- CreationQuizz(Android).java
+++ CreationQuizz(Android).java
@@ -2,6 +2,7 @@ package ceri.createquizz;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 
 import static ceri.createquizz.R.id.checkBox_mdp;
+import static ceri.createquizz.R.id.checkBox_time;
 
 public class Createquizz extends AppCompatActivity {
     @Override
@@ -20,7 +22,7 @@ public class Createquizz extends AppCompatActivity {
     }
 
 
-    public void affichage_PopUp(View v)
+    public void affichage_PopUp_mdp(View v)
     {
         CheckBox checkboxmdp = (CheckBox) findViewById(checkBox_mdp);
         if(checkboxmdp.isChecked())
@@ -28,6 +30,16 @@ public class Createquizz extends AppCompatActivity {
             ShowMotDePasse();
         }
     }
+
+    public void affichage_PopUp_time(View v)
+    {
+        CheckBox checkboxtime = (CheckBox) findViewById(checkBox_time);
+        if(checkboxtime.isChecked())
+        {
+            ShowTime();
+        }
+    }
+
     private void ShowMotDePasse() {
         AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
 
@@ -48,5 +60,32 @@ public class Createquizz extends AppCompatActivity {
         helpDialog.show();
         Button ok =helpDialog.getButton(DialogInterface.BUTTON_POSITIVE);
         ok.setTextColor(Color.BLUE);
+    }
+
+    private void ShowTime() {
+        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View timelayout = inflater.inflate(R.layout.popupminuteur, null);
+        helpBuilder.setView(timelayout);
+
+        helpBuilder.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing but close the dialog
+                    }
+                });
+
+        // Remember, create doesn't show the dialog
+        AlertDialog helpDialog = helpBuilder.create();
+        helpDialog.show();
+        Button ok =helpDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        ok.setTextColor(Color.BLUE);
+    }
+
+    public void modif_questionnaire(View view) {
+
+        startActivity(new Intent(this,Modifquizz.class));
     }
 }
