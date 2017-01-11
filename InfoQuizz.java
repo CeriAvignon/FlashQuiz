109de68deca0895c23856e0ffe93ec6af02b4425
ChambonBrice <Brice.chambon@outlook.com>
2017-01-11 17:07:23 +0100

    Create InfoQuizz.java

---
+++ InfoQuizz.java
@@ -0,0 +1,22 @@
+package ig.flash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onclick(View v) {
+        startActivity(new Intent(this, StartSession.class));
+    }
+}
