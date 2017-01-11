8f51c9d2699be7eea363b0e184dab830e28a27df
Benoit Dorey <benoitdorey@gmail.com>
2017-01-11 10:07:21 +0100

    Update and rename stat_user.java to statUser.java

--- src/statUser.java
+++ src/statUser.java
@@ -1,21 +1,21 @@
 /**
- * Stat_user
+ * StatUser
  *
  * Produce statistics from the question's answers.
  *
  * @author Benoît Dorey
  */
-public class stat_user {
+public class statUser {
 
 	/**
 	 * Function who calculate statistic of the User for more than one session
 	 *
 	 *
-	 * @param session_number
+	 * @param sessionNumber
 	 *            Int who is the number of session for the User
-	 * @param Stat_true
+	 * @param StatTrue
 	 *            Double array that contain all of the User's true stat for each
-	 *            session like: Stat_true[20,70,90] for 20%, 70% and 90% true
+	 *            session like: StatTrue[20,70,90] for 20%, 70% and 90% true
 	 *            answers in 3 sessions
 	 * @return Double array that contain statistics values for good and bad
 	 *         answers, like: Res[0] = 45 and Res[1] = 55; so 45% true and 55%
@@ -23,16 +23,16 @@ public class stat_user {
 	 */
 	// Fonction pour calculer le pourcentage de réussite de l'utilisateur sur
 	// une certain nombre de session
-	public static double[] stat_resultUserGeneral(int session_number,
-			double[] Stat_true) {
+	public static double[] statResultUserGeneral(int sessionNumber,
+			double[] StatTrue) {
 
 		// Tableau avec le pourcentage de juste et de faux
 		double Res[] = { 0, 0 };
 
-		for (int i = 0; i < session_number; i++) {
-			Res[0] = Res[0] + Stat_true[i];
+		for (int i = 0; i < sessionNumber; i++) {
+			Res[0] = Res[0] + StatTrue[i];
 		}
-		Res[0] = Res[0] / session_number;
+		Res[0] = Res[0] / sessionNumber;
 
 		Res[1] = 100 - Res[0];
 
@@ -47,10 +47,10 @@ public class stat_user {
 	 * /** Function who calculate statistic of the User for one session
 	 *
 	 *
-	 * @param res_true
+	 * @param resTrue
 	 *            Int array that contain the number of good answer for the User
 	 *            in the session
-	 * @param res_false
+	 * @param resFalse
 	 *            Int array that contain the number of bad answer for the User
 	 *            in the session
 	 * @return Double array that contain statistics values for good and bad
@@ -59,16 +59,16 @@ public class stat_user {
 	 */
 	// Fonction pour calculer le pourcentage de réussite de l'utilisateur sur
 	// une session
-	public static double[] stat_resultUser(int res_true, int res_false) {
+	public static double[] statResultUser(int resTrue, int resFalse) {
 		// Total des votes
-		double total = res_true + res_false;
+		double total = resTrue + resFalse;
 		// Tableau avec le pourcentage de juste et de faux
 		double Res[] = { 0, 0 };
 
 		// Pourcentage de réponses vraies
-		Res[0] = (res_true * 100) / total;
+		Res[0] = (resTrue * 100) / total;
 		// pourcentages de réponses fausses
-		Res[1] = (res_false * 100) / total;
+		Res[1] = (resFalse * 100) / total;
 
 		// Tableau des pourcentages
 		return Res;
@@ -79,27 +79,27 @@ public class stat_user {
 	 */
 	// Main de test des deux fonctions et de présentation des entrées
 	//
-	// stat_resultUser demande le nombre de bonne réponses et de mauvaises sur
+	// statResultUser demande le nombre de bonne réponses et de mauvaises sur
 	// une session
 	// afin de calculer les pourcentages de bonnes réponses et de mauvaises sur
 	// ce même sondage.
 	//
-	// stat_resultUserGeneral demande le nombre de sondage auquel a répondu
+	// statResultUserGeneral demande le nombre de sondage auquel a répondu
 	// l'utilisateur et
 	// les pourcentages de réussites à ces sondages.
 	//
 	//
 	public static void main(String[] args) {
-		// Test fonction stat_resultUser
-		double[] Res = stat_resultUser(45641, 12737);
+		// Test fonction statResultUser
+		double[] Res = statResultUser(45641, 12737);
 
 		System.out
 				.println("Res[0] = " + Res[0] + "% \nRes[1] = " + Res[1] + "%");
 
-		// Test fonction stat_resultUserGeneral
-		double[] Stat_true = { 56.9, 36.3, 60.3, 25.2 };
+		// Test fonction statResultUserGeneral
+		double[] StatTrue = { 56.9, 36.3, 60.3, 25.2 };
 
-		double[] Res2 = stat_resultUserGeneral(4, Stat_true);
+		double[] Res2 = statResultUserGeneral(4, StatTrue);
 
 		System.out.println(
 				"Res2[0] = " + Res2[0] + "% \nRes2[1] = " + Res2[1] + "%");
