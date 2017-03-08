77d98bff8a8e18c84601ad6cf2e84e07fc1fea80
DemogueBruno <Faellyss@users.noreply.github.com>
2017-03-08 16:39:38 +0100

    Add files via upload

--- src/ActionList.java
+++ src/ActionList.java
@@ -1,3 +1,17 @@
-public class ActionList
+class ActionDeleteList
 {
+/**
+* Fonction de supression de question d'une liste 
+*
+* @author BrunoDemogue
+* @param idl= id de la liste a suprimer
+*/
+
+
+public static void Supressionliste(idl)
+	{
+		SupressionSessionList(idl);			// fonction bdd qui reçoie un  id de liste et supprime le lien entre les Session et cette liste
+		SupressionQuestionList(idl);		// fonction bdd qui reçoie un  id de liste et supprime le lien entre les question et cette liste
+		SupressionListe(idl);				// supression de la liste dans la bdd
+	}
 }
