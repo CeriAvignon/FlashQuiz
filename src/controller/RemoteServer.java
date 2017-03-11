2e90c99bf22e1ba412c2d18ce9fe3fd9d9749a04
Julien Boge <julien.boge@alumni.univ-avignon.fr>
2017-03-11 16:59:13 +0100

    ctrl/RemoteServer: 'server' renommé en 'socket'

--- src/controller/RemoteServer.java
+++ src/controller/RemoteServer.java
@@ -14,14 +14,14 @@ import lib.net.*;
 //=============================================================================
 public abstract class RemoteServer
 {
-	private static RemoteServerSocket server;
+	private static RemoteServerSocket socket;
 
 	//---------------------------------------------------------------------------
 	// * Is connected to server
 	//---------------------------------------------------------------------------
 	public static Boolean isConnected()
 	{
-		return (server != null);
+		return (socket != null);
 	}
 
 	//---------------------------------------------------------------------------
@@ -30,7 +30,7 @@ public abstract class RemoteServer
 	//---------------------------------------------------------------------------
 	public static void connect(Integer port)
 	{
-		if(server != null) {
+		if(socket != null) {
 			Console.printError("déjà connecté à un serveur!");
 			return;
 		}
@@ -40,13 +40,13 @@ public abstract class RemoteServer
 		}
 		Console.print("Demandé à rejoindre le serveur sur le port " + port + "...");
 		try {
-			Socket socket = new Socket("localhost", port);
-			server        = new RemoteServerSocket(socket);
-			server.start();
+			Socket simpleSocket = new Socket("localhost", port);
+			socket = new RemoteServerSocket(simpleSocket);
+			socket.start();
 		} catch (IOException e) {
 			// e.getMessage();
 			// e.printStackTrace();
-			Console.print("Impossible de se connecter au serveur");
+			Console.printError("Impossible de se connecter au serveur");
 		}
 	}
 
@@ -56,9 +56,9 @@ public abstract class RemoteServer
 	public static void close()
 	{
 		if(!isConnected()) return;
-		server.close();
+		socket.close();
 		Console.print("Déconnexion du serveur!");
-		server = null;
+		socket = null;
 	}
 
 	//---------------------------------------------------------------------------
@@ -79,7 +79,7 @@ public abstract class RemoteServer
 	public static void sendRequest(String action, Object object)
 	{
 		if(!checkIsConnected()) return;
-		server.sendRequest(action, object);
+		socket.sendRequest(action, object);
 	}
 
 	//---------------------------------------------------------------------------
