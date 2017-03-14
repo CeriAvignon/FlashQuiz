b8723b10ec7efaef4310c7992864843080aeff98
ChambonBrice <Brice.chambon@outlook.com>
2017-03-14 11:55:18 +0100

    Add files via upload

---
+++ src/Android/StartSession.java
@@ -0,0 +1,14 @@
+package ig.flash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartSession extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_session);
+    }
+
+}
