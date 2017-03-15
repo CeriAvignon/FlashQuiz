d121b610719a0248262ccd7f8cb198d2b4e9b8e2
nioua abdeljalil <nioua.abdeljalil@gmail.com>
2017-03-15 16:54:26 +0100

    Update ActionMedia.java

--- src/ActionMedia.java
+++ src/ActionMedia.java
@@ -1,3 +1,17 @@
-public class ActionMedia
-{
+
+public class ActionMedia {
+
+		public Media MediaInsert(String MediaSource) throws ClassNotFoundException, Exception{
+		    Media media=new Media(MediaSource);
+			  Media.DefineMediaInfos();
+			  Media.InsertMedia();
+		    return media;
+		}
+		public Media MediaDelete(String MediaLocation) throws ClassNotFoundException, Exception {
+			  Media media = new Media();
+			  Media.MediaLocation=MediaLocation;
+			  Media.DeleteMedia();
+			  return media;
+		}
+	
 }
