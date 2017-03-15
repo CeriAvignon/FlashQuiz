80dccb90742b38c3e8101d19a687e87c674ebfab
anaskharmouch <anaskharmouch1@gmail.com>
2017-03-15 14:34:49 +0100

    Update ActionQuestion.java

--- src/ActionQuestion.java
+++ src/ActionQuestion.java
@@ -2,16 +2,13 @@ public class ActionQuestion
 {
   
   
-  public void SupprimerQuestion()
+  public void SupprimerQuestion(int id)
     
     
     
   {
-  
-  
-  
-  
-  
+          deleteQuestion(int id) ;
+    
   }
   
 }
