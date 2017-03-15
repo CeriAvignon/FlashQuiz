6fbafa543f6640d475b74c28f900becce70c4777
nioua abdeljalil <nioua.abdeljalil@gmail.com>
2017-03-15 16:50:51 +0100

    Update ActionQuestion.java

--- src/ActionQuestion.java
+++ src/ActionQuestion.java
@@ -1,3 +1,9 @@
 public class ActionQuestion
 {
+  
+    Question QuestionInsert(int QuestType, String QuestConten, boolean MediaExist) throws ClassNotFoundException, Exception {
+		     Question quest=new Question (QuestType,QuestConten,MediaExist);
+		     return quest;
+	  }
+  
 }
