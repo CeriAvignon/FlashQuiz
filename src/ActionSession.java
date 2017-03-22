c5fe675fea12ee5161b53f484860efba5b4a216b
DemogueBruno <Faellyss@users.noreply.github.com>
2017-03-22 16:26:56 +0100

    Add files via upload

--- src/ActionSession.java
+++ src/ActionSession.java
@@ -1,3 +1,15 @@
 public class ActionSession
 {
+/**
+* Fonction de supression
+*
+* @author BrunoDemogue
+* @param ids=id de la session a suprimer
+*/
+	public  void SupressionSession(ids)
+	{
+		SupressionListsession(ids);			
+		SupressionSession(ids);			
+	}
+
 }
