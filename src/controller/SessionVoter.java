d927eb1b044ad631b61426fb91e33d76eb37b823
Jean-loup Gaussen <j-loup.gaussen@laposte.net>
2017-04-04 11:59:23 +0200

    object cast checking + startQuestion() + @see comments

--- src/controller/SessionVoter.java
+++ src/controller/SessionVoter.java
@@ -2,6 +2,9 @@ package controller;
 
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.Timer;
+
 import lib.display.*;
 import model.base.*;
 import model.session.*;
@@ -12,6 +15,8 @@ import model.session.*;
  * is given by the voter.
  * 
  * @author Jean-Loup
+ * 
+ * @see SessionMaster
  *
  */
 public abstract class SessionVoter {
@@ -22,42 +27,92 @@ public abstract class SessionVoter {
 	// private static String name;
 
 	/**
-	 * A list of the voter answers, the integer represent the id of the voter
-	 * answer.
+	 * A list of the voter answers, the integer represent the id of the question
+	 * answered.
+	 * 
+	 * @see VoterAnswer
 	 */
 	private static Map<Integer, VoterAnswer> voterAnswers;
 
 	/**
 	 * The list of all the question list the session have.
+	 * 
+	 * @see QuestionList
 	 */
 	private static List<QuestionList> lists;
 
 	/**
 	 * The id of the list which the current question came from.
+	 * 
+	 * @see QuestionList
 	 */
 	private static Integer currListId;
 
 	/**
 	 * The id of the question currently asked.
+	 * 
+	 * @see Question
 	 */
 	private static Integer currQuestionId;
 
 	/**
-	 * The command interpreter, execute an action on an object.
+	 * A timer used when a question have a time limit.
+	 */
+	private Timer timer;
+
+	/**
+	 * The command interpreter, execute an action on an object. Mostly used when
+	 * the session master need to make the voter session to do something.
 	 * 
 	 * @param action
 	 *            The action wanted on the object
 	 * @param object
 	 *            The object on which the action is made
 	 * @return True if the action was found, False if not
+	 * 
+	 * @see SessionVoter#initialize(List)
+	 * @see SessionVoter#startQuestion(Integer, Integer)
 	 */
+	@SuppressWarnings("unchecked")
 	public static Boolean interpreter(String action, Object object) {
 		switch (action) {
 		case "printReceived":
 			printReceived((String) object);
 			break;
 		case "initialize":
-			initialize((List<QuestionList>) object); // %TODO%
+			// Check if the object is a List
+			if (object instanceof List<?>) {
+				// Check if every elements of this list is a QuestionList
+				for (Object tmp : (List<?>) object) {
+					if (!(tmp instanceof QuestionList)) {
+						return false;
+					}
+				}
+				initialize((List<QuestionList>) object);
+			} else {
+				return false;
+			}
+			break;
+		case "startQuestion":
+			if (object instanceof List<?>) {
+				for (Object tmp : (List<?>) object) {
+					if (!(tmp instanceof Integer)) {
+						return false;
+					}
+				}
+				List<Integer> id = (List<Integer>) object;
+				if (id.size() == 2) {
+					currListId = id.get(0);
+					currQuestionId = id.get(1);
+					startQuestion(currListId, currQuestionId);
+				} else {
+					return false;
+				}
+
+			} else {
+				return false;
+			}
+
 			break;
 		default:
 			return false;
@@ -70,6 +125,8 @@ public abstract class SessionVoter {
 	 * 
 	 * @param s
 	 *            The string received
+	 * 
+	 * @see Ansi
 	 */
 	public static void printReceived(String s) {
 		Console.printAsync(Ansi.GREEN + "Reçu: " + Ansi.RESET + s);
@@ -80,6 +137,9 @@ public abstract class SessionVoter {
 	 * 
 	 * @param line
 	 *            The line to capitalise
+	 * 
+	 * @see RemoteServer
+	 * @see RemoteServer#sendRequest(String, Object)
 	 */
 	public static void capitalize(String line) {
 		RemoteServer.sendRequest("capitalize", line);
@@ -93,12 +153,15 @@ public abstract class SessionVoter {
 	 *            The session name
 	 * @param list
 	 *            The question list of the session
+	 * 
+	 * @see QuestionList
+	 * @see SessionVoter#lists
 	 */
 	public static void initialize(/* String name, */ List<QuestionList> lists) {
 		SessionVoter.lists = lists;
 		// SessionVoter.name = name;
 
-		// display view %TODO%
+		// display_waiting_view (IG) %TODO%
 	}
 
 	/**
@@ -112,7 +175,11 @@ public abstract class SessionVoter {
 	 *            The id of the question in the list
 	 */
 	public static void startQuestion(Integer listId, Integer questionId) {
-		// %TODO%
+		// getList(listId); (BDD) %TODO%
+		// getQuestion(questionId); (BDD) %TODO%
+
+		// display_question_view(); (IG) %TODO%
+
 	}
 
 	/**
