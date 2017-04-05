95b7af464d820c03c6868f75c13bafc473608210
DemogueBruno <Faellyss@users.noreply.github.com>
2017-04-05 16:50:00 +0200

    Update List.java

--- src/List.java
+++ src/List.java
@@ -1,19 +1,15 @@
-class List
+import model.base*;
+
+class List extend ListBase
 {
-	private int id; 									// id
-	private String name; 								// nom
-	private int author;									// id auteur
-	private int question[];								// question
+	public int question[];								// question
 
 /**
 * constructeur vide de liste 
-*
 */
 	public List()
 	{
-		this.id=0;
-		this.name="";
-		this.author=0;
+		super();
 		this.question=null;
 	}
 
@@ -27,97 +23,10 @@ class List
 */
 	public List(int id,String name,int author,int question[])
 	{
-		this.id=id;
-		this.namelist=name;
-		this.author=author;
+		super(id,name,author);
 		for(int i=0;i<question.lenght;i++)
 		{
 			this.question[i]=question[i];
-		}
-	}
-
-/**
-* getter de l'atribut id
-*
-* @return retourne la valeur de l'atribut id
-*/
-	public int getId()
-	{
-		return this.id;
-	}
-
-/**
-* getter de l'atribut name
-*
-* @return retourne la valeur de l'atribut name
-*/
-	public String getName()
-	{
-		return this.name;
-	}
-
-/**
-* getter de l'atribut author
-*
-* @return retourne la valeur de l'atribut author
-*/
-	public int getAuthor()
-	{
-	return this.author;
-	}
-
-/**
-* getter de l'atribut question
-*
-* @return retourne la valeur de l'atribut question
-*/
-	public int[] getQuestion()
-	{
-		return this.question;
-	}
-
-/**
-* setter de l'atribut id
-*
-* @param id = valeur à atribuer à id
-*/
-	public void setId(int id)
-	{
-		this.id = id;
-	}
-
-/**
-* setter de l'atribut name
-*
-* @param name = valeur à atribuer à name 
-*/
-	public void setName(String name)
-	{
-		this.name= name;
-	}	
-
-/**
-* setter de l'atribut author
-*
-* @param author = valeur à atribuer à author
-*/
-	public void setAuthor(int author)
-	{
-		this.author = author;
-	}	
-	
-/**
-* setter de l'atribut list
-*
-* @param question = tableau de valeur à atribuer à question
-*/
-	public void setQuestion(int question [])
-	{
-		int i=0;
-		while(question[i]!=null)
-		{
-			this.question[i] = question[i];
-			i++;
 		}
 	}
 }
