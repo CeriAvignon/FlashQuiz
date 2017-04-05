865f4f89a75fe5bad4286a6c3a0e4a18d3c7e542
Olfar <plow006@gmail.com>
2017-04-05 14:55:56 +0200

    Update stat2Activity.java

--- src/Android/stat2Activity.java
+++ src/Android/stat2Activity.java
@@ -7,12 +7,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import java.util.ArrayList;
 
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 
 public class stat2Activity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stat2);
 
@@ -22,6 +31,7 @@ public class stat2Activity extends Activity {
         TableLayout table = (TableLayout) findViewById(R.id.idTable);
         TableRow row;
         TextView cel1, cel2; // création des cellules
+
 
 // Boucle qui fait chaque ligne
         for (int i = 0; i < col1.length; i++) {
@@ -42,5 +52,48 @@ public class stat2Activity extends Activity {
             row.addView(cel2);
             table.addView(row);
         }
+
+// barchart
+        HorizontalBarChart chart = (HorizontalBarChart) findViewById(R.id.chart);
+        chart.setContentDescription("");
+        BarData data = new BarData(getDataSet());
+        chart.setData(data);
+        chart.animateXY(2000, 2000);
+        chart.invalidate();
+
+        YAxis left = chart.getAxisLeft();
+        left.setDrawLabels(false); // no axis labels
+        left.setDrawAxisLine(false); // no axis line
+        left.setDrawGridLines(false); // no grid lines
+        left.setDrawZeroLine(true); // draw a zero line
+        chart.getAxisRight().setEnabled(false); // no right axis
+        chart.setDrawValueAboveBar(false);
+
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawLabels(false); // no axis labels
+        xAxis.setDrawAxisLine(false); // no axis line
+        xAxis.setDrawGridLines(false); // no grid lines
+        chart.getXAxis().setEnabled(false);
+
     }
+
+    private BarDataSet getDataSet() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 0));
+        entries.add(new BarEntry(1f, 85));
+        entries.add(new BarEntry(2f, 65));
+
+        BarDataSet dataSet = new BarDataSet(entries,"bonne reponses");
+        dataSet.setDrawValues(true);
+        dataSet.setValueFormatter(new PercentFormatter());
+        return dataSet;
+    }
+/*
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("info1");
+        return labels;
+    }
+*/
 }
