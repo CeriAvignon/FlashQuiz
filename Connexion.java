b4d1d05463470d2ce27af22330a68c545727d6bb
Huot Amaury <Le-fou-volant12@hotmail.com>
2017-04-05 16:24:01 +0200

    Update Connexion.java

--- Connexion.java
+++ Connexion.java
@@ -1,64 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package connexion;
-
-/**
- *
- * @author uapv1501158
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class Connexion {
 
-    public static void main(String[] args) {
+public class ConnexionJav {
+   public static void main( String args[] )
+     {
+       Connection c = null;
+       Statement stmt = null;
+       try {
+       Class.forName("org.postgresql.Driver");
+         c = DriverManager.getConnection("jdbc:postgresql://pedago.univ-avignon.fr/flashquiz", "flashquiz", "FiBZJ2DA");
+         c.setAutoCommit(false);
+         System.out.println("Opened database successfully");
 
-        Connection con = null;
-        Statement st = null;
-        ResultSet rs = null;
-
-    
-        String url = "jdbc:postgresql://pedago.univ-avignon.fr/flasquiz";//declaration des variables utiles à la connection
-        String user = "flashquiz";
-        String password = "FiBZJ2DA";
-
-        try {
-            con = DriverManager.getConnection(url, user, password);
-            st = con.createStatement();
-            rs = st.executeQuery("SELECT VERSION()");
-
-            if (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-
-        } catch (SQLException ex) {
-            Logger lgr = Logger.getLogger(Version.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-
-        } finally {
-            try { //close
-                if (rs != null) {
-                    rs.close();
-                }
-                if (st != null) {
-                    st.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
-
-            } catch (SQLException ex) {
-                Logger lgr = Logger.getLogger(Version.class.getName());
-                lgr.log(Level.WARNING, ex.getMessage(), ex);
-            }
-        }
-    }
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery( "SELECT * FROM answer" );
+ 
+         rs.close();
+         stmt.close();
+         c.close();
+       } catch ( Exception e ) {
+         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+         System.exit(0);
+       }
+       System.out.println("Operation done successfully");
+     }
 }
