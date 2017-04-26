ad416071fbd172a348dcb4a261b90ba0a1957bbe
Sacha Le Doeuff <sacha.ledoeuff@hotmail.fr>
2017-04-26 17:46:52 +0200

    Update VoterAnswerHandler.java

--- src/VoterAnswerHandler.java
+++ src/VoterAnswerHandler.java
@@ -4,7 +4,7 @@ import model.base.*;
 import model.session.*;
 
 
-	/**
+/**
 	*  @author Sacha Le Doeuff
 	*  @version 1.0
 	*  Kit de fonction qui permet de recuperer les reponses des utilisateurs
@@ -22,19 +22,17 @@ public class VoterAnswerHandler {
 	 **/
 	public void setVoterAnswer(int id_question, int id_session, int id_user, String texte)
 	{
-		Connexion cnx;
+		Connexion cnx;					//on ouvre une connexion
 
-		/**
-		 * Creation d'une requete vide qui sera affectee selon
-		 * les besoins de la fonction.
-		 * */
+		String query="";				//requete vide
+		PreparedStatement prepare = cnx.prepareStatement();			//prends la requete pour la modifier puis l'envoyer
 
-		String query="";
-		PreparedStatement prepare = cnx.prepareStatement();
-		Statement statement = cnx.createStatement();
+		//si besoin de complexifier la requete
+		//Statement statement = cnx.createStatement();
 
 		try{
 			query="INSERT INTO VoterAnswer(ID_Session, ID_Question, ID_User, Content_FreeAnswer) VALUES(?,?,?,?);";
+			//on remplace les valeur manquante de la requete, puis on envoi la requete
 			prepare = cnx.prepareStatement(query);
 			prepare.setObject(1,id_session);
 			prepare.setObject(2,id_question);
@@ -43,8 +41,8 @@ public class VoterAnswerHandler {
 			prepare.executeUpdate();
 			prepare.close();
 		}
-		catch(SQLException e)
-		{
+		catch(SQLException e){
+			//En cas d'erreur
 			e.printStackTrace();
 		}
 		finally{
@@ -66,9 +64,8 @@ public class VoterAnswerHandler {
 
 		try{
 			PreparedStatement prepare = cnx.prepareStatement();
-			Statement statement = cnx.createStatement();
 			ResultSet res;
-			String query="";
+			String query="";	//La ou on met le resultat de la requete
 
 			query="SELECT * FROM VoterAnswer WHERE ID_Session=? AND ID_Question=? AND ID_User=?;";
 
@@ -82,7 +79,7 @@ public class VoterAnswerHandler {
 			res.close();
 
 			Object resultat[][] = res;
-			return resultat;
+			return resultat;			//retour de la requete
 		}
 		catch(SQLException e)
 		{
@@ -107,7 +104,6 @@ public class VoterAnswerHandler {
 
 		try{
 			PreparedStatement prepare = cnx.prepareStatement();
-			Statement statement = cnx.createStatement();
 			ResultSet res;
 			String query="";
 
@@ -147,7 +143,6 @@ public class VoterAnswerHandler {
 
 		try{
 			PreparedStatement prepare = cnx.prepareStatement();
-			Statement statement = cnx.createStatement();
 			ResultSet res;
 			String query="";
 
