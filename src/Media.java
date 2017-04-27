cbfe4f11bec120d65b9d1b18df180630969fb5c9
nioua abdeljalil <nioua.abdeljalil@gmail.com>
2017-04-27 09:35:22 +0200

    Update Media.java

--- src/Media.java
+++ src/Media.java
@@ -1,8 +1,14 @@
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.UUID;
+
 class Media
 {
-	private int id; 							
-	private String url;										
-	private int type; 	
+	private int id; 
+	static   Integer MediaType;//Représente le type du media, 0 => image, 1 => audio, 2 si => vidéo 
+	static   String MediaSource;//la source du Media 
+	static	  String MediaLocation;// Chemin local dans la base de données
 
 	/**
 * getter de l'atribut id
@@ -13,24 +19,8 @@ class Media
 	{
 		return this.id;
 	}
-/**
-* getter de l'atribut url
-*
-* @return retourne la valeur de l'atribut url
-*/
-	public String getUrl()
-	{
-		return this.url;
-	}
-	/**
-* getter de l'atribut type
-*
-* @return retourne la valeur de l'atribut type
-*/
-	public int getType()
-	{
-		return this.type;
-	}
+
+
 /**
 * setter de l'atribut id
 *
@@ -40,25 +30,62 @@ class Media
 	{
 		this.id = id;
 	}
-/**
-* setter de l'atribut url
-*
-* @param url = valeur à atribuer à url 
-*/
-	public void setUrl(String url)
-	{
-		this.url= url;
+	public static Integer getMediaType() {
+		return MediaType;
 	}
-/**
-* setter de l'atribut type
-*
-* @param type = valeur à atribuer à type
-*/
-	public void setId(int type)
-	{
-		this.type = type;
-	}	
+	public static void setMediaType(Integer mediaType) {
+		MediaType = mediaType;
+	}
+	public static String getMediaSource() {
+		return MediaSource;
+	}
+	public static void setMediaSource(String mediaSource) {
+		MediaSource = mediaSource;
+	}
+	public static String getMediaLocation() {
+		return MediaLocation;
+	}
+	public static void setMediaLocation(String mediaLocation) {
+		MediaLocation = mediaLocation;
+	}
+	
+	//Constructeur
+	Media( String MediaSource) {
+		Media.MediaType=0;
+		Media.MediaSource=MediaSource;
+		Media.MediaLocation="..\\flachquiz\\media\\";
+	}
+	
+	public static void defineMediaInfos() throws Exception {
+        	Path path = Paths.get(MediaSource);
+        	String MediaTyp = Files.probeContentType(path).substring(0,5);
+        	switch (MediaTyp) {
+        		case "image" :MediaType=0; break;
+        		case "audio" :MediaType=1; break;
+        		case "video" :MediaType=2; break;
+        	}
+		MediaLocation+=MediaTyp+"\\"+UUID.randomUUID().toString()+MediaSource.substring(MediaSource.length()-4);
+		 //ex: "..\\flachquiz\\media\\audio\\43971bda-851b-4130-b9f2-f496b1962ccc.mp3"
+  	 }
+	 public static void uploadMedia()  throws ClassNotFoundException, Exception{
+		  File Source=new File(MediaSource);
+		  File Destination = new File(Media.MediaLocation);
+		  FileInputStream fileInputStream = new FileInputStream(Source);
+		  FileOutputStream fileOutputStream = new FileOutputStream(Destination);
+		  int bufferSize;
+		  byte[] bufffer = new byte[512];
+		  while ((bufferSize = fileInputStream.read(bufffer)) > 0) {
+		        fileOutputStream.write(bufffer, 0, bufferSize);
+		  }  
+		  fileInputStream.close();
+		  fileOutputStream.close();
+	 }
+	
 
+	 public static void deleteMedia()  throws ClassNotFoundException, Exception{
+	 	Path Location=Paths.get(MediaLocation); 
+		Files.delete(Location);
+	 }
 				
 
 }
