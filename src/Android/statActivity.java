a08bdafe06d9d75f4b8adeeb7a78a358a7c1dc8f
Guichard Yoan <plow006@gmail.com>
2017-04-27 17:25:42 +0200

    Update and rename MenuStat.java to statActivity.java

--- src/Android/statActivity.java
+++ src/Android/statActivity.java
@@ -1,4 +1,4 @@
-package yoan.example.com.flashquizz;
+package ig.flash;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
-public class statActivity extends Activity {
+public class statActivity extends MainActivity {
     /**
      * Called when the activity is first created.
      */
