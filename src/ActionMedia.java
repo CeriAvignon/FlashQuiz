fb2a777abfcfad86f40e839a3eae01248e8223a3
nioua abdeljalil <nioua.abdeljalil@gmail.com>
2017-04-27 09:36:13 +0200

    Update ActionMedia.java

--- src/ActionMedia.java
+++ src/ActionMedia.java
@@ -1,3 +1,17 @@
-public class ActionMedia
-{
+public class ActionMedia {
+            	//Recupere le chemin du media pour l'Upload (Interface graphique)
+		public Media mediaUpload(String MediaSource) throws ClassNotFoundException, Exception{
+		          Media media=new Media(MediaSource);
+			  Media.defineMediaInfos();
+			  Media.uploadMedia();
+		          return media;
+		}
+		//Recupere le chemin du media dans la base de données (BD)
+		public Media mediaDelete(String MediaLocation) throws ClassNotFoundException, Exception {
+			  Media media = new Media();
+			  Media.MediaLocation=MediaLocation;
+			  Media.deleteMedia();
+			  return media;
+		}
+	
 }
