bce20889dd6da01f0b3b0bc6b3912681591c1837
Guichard Yoan <plow006@gmail.com>
2017-04-27 17:20:39 +0200

    Update AcceuilActivity.java

--- src/Android/AcceuilActivity.java
+++ src/Android/AcceuilActivity.java
@@ -1,4 +1,4 @@
-package com.example.yoan.flashquizz;
+package ig.flash;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.Switch;
 
 
-public class acceuilActivity extends Activity{
-
+public class acceuilActivity extends MainActivity{
+    Switch switch_logout;
 Button newQuizz=null;
 Button participQuizz=null;
 Button stat=null;
@@ -31,13 +32,25 @@ Button reglage=null;
         participQuizz.setOnClickListener(participQuizzListener);
         stat.setOnClickListener(statListener);
         reglage.setOnClickListener(reglageListener);
+        switch_logout = (Switch) findViewById(R.id.switch_logout);
+
+        //redirection vers la page d'accueil
+        switch_logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+
+                Intent myIntent = new Intent(acceuilActivity.this, loginActivity.class);
+                startActivity(myIntent);
+            }
+
+        });
     }
     
     // Listener du boutton créer un quizz
     private OnClickListener newQuizzListener = new OnClickListener() {
         @Override
         public void onClick(View v1) {
-        	Intent newquizz = new Intent(acceuilActivity.this, nouveauquizzActivity.class);
+        	Intent newquizz = new Intent(acceuilActivity.this, ImportActivity.class);
         	startActivity(newquizz);
         }
       };
@@ -46,7 +59,7 @@ Button reglage=null;
      private OnClickListener participQuizzListener = new OnClickListener() {
           @Override
           public void onClick(View v2) {
-          	Intent participquizz = new Intent(acceuilActivity.this, participQuizzActivity.class);
+          	Intent participquizz = new Intent(acceuilActivity.this, RechercheQuizz.class);
           	startActivity(participquizz);
           }
         };
@@ -64,9 +77,11 @@ Button reglage=null;
      private OnClickListener reglageListener = new OnClickListener() {
               @Override
               public void onClick(View v4) {
-                Intent reglagequizz = new Intent(acceuilActivity.this, reglageActivity.class);
+                Intent reglagequizz = new Intent(acceuilActivity.this, Reglage.class);
                 startActivity(reglagequizz);
               }
             };
+
+
 
 }
