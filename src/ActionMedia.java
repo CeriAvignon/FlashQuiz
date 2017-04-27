e3fcbfd324e0f7c2c411beca4fc5b5bfe7427a8a
nioua abdeljalil <nioua.abdeljalil@gmail.com>
2017-04-27 09:28:53 +0200

    Update ActionMedia.java

--- src/ActionMedia.java
+++ src/ActionMedia.java
@@ -1,3 +1,10 @@
 public class ActionMedia
 {
+    //Recupere le chemin du media dans la base de données (BD)
+		public Media mediaDelete(String MediaLocation) throws ClassNotFoundException, Exception {
+			  Media media = new Media();
+			  Media.MediaLocation=MediaLocation;
+			  Media.deleteMedia();
+			  return media;
+		}
 }
