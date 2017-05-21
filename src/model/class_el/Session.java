fab327a0a80d1efc0ea7a3d460ae8cf69851891d
Le Veve Mathieu <levevemathieu@gmail.com>
2017-05-21 12:06:03 +0200

    Update Session.java

--- src/model/class_el/Session.java
+++ src/model/class_el/Session.java
@@ -2,10 +2,15 @@ package model.class_el;
 import java.util.Date;
 import model.base.*;
 
+// Cette classe est une extend de SessionBase du package model.base.Session
 public class Session extends SessionBase
 {	
 	public int list[];	
-
+/**
+* constructeur vide de liste
+* @author: LE VEVE Mathieu
+* @brief: construit un objet vide, instancié lors d'un click sur un bouton
+*/
 	public Session ()		
 	{	
 		this.id=0;
@@ -17,7 +22,18 @@ public class Session extends SessionBase
 		this.name="";
 		this.list=null;
 	}
-
+/**
+* constructeur d'une session
+* @author: LE VEVE Mathieu
+* @brief : constructeur d'une session paramétrable
+* @param id = valeur à attribuer à id d'une session
+* @param startingDate = date de début paramétrable d'une session
+* @param endingDate = date de fin paramétrable d'une session
+* @param password = mot de passe à paramétrer pour limiter l'accès (paramètre session non libre)
+* @param type =  différencie session libre/ fermée
+* @param name = nom d'une session
+* @param list = liste d'identifiants de questionnaires (listes de question ou id d'objet List()]
+*/
 	public Session (int id, Date startingDate, Date endingDate, String password,boolean type, String name, int list[])		
 	{	
 		this.id=id;
