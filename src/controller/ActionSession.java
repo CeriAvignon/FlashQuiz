ad68158d6108a1c34df1418cdb81725bdfb7c80d
Le Veve Mathieu <levevemathieu@gmail.com>
2017-05-21 12:23:01 +0200

    Update ActionSession.java

--- src/controller/ActionSession.java
+++ src/controller/ActionSession.java
@@ -8,11 +8,13 @@ public class ActionSession
 	Session session;
 
 /**
-* Send a session at RES-BDD
+* @brief Send a session at RES-BDD
 *
 * @author Mathieu Le Veve
 * @param no param
+* @return void
 */
+	
 	public static void submitSession()
 	{
 		sendSession(session);
@@ -20,7 +22,12 @@ public class ActionSession
 	}
 
 
-
+/**
+* @brief Init Session object with an ID session
+*
+* @author Mathieu Le Veve
+* @param ids
+*/
 	public void initSession(int ids)
 	{
 		ResultSet res = getSession(ids);
@@ -46,58 +53,128 @@ public class ActionSession
 			list);
 	}
 
-
+/**
+* @brief Create a session when a user click on the IG Button
+*
+* @author Mathieu Le Veve
+* @param no param
+* @return void
+*/
 	public void createSession()
 	{
 		session = new Session();	
 	}
 
-	
+/**
+* @brief Alter session name with a new name
+*
+* @author Mathieu Le Veve
+* @param name = Name of the modified object
+* @return void
+*/	
 	public void modifSessionTitle(String name)
 	{
 		setTitle(name);
 	}
 
+/**
+* @brief Alter session open date with a new open date
+*
+* @author Mathieu Le Veve
+* @param startingDate = Open date of the modified object
+* @return void
+*/
 	public void modifSessionOpen(Date startingDate)
 	{
 		setOpen(startingDate);
 	}
 
+/**
+* @brief Alter session ending date with a new ending date
+*
+* @author Mathieu Le Veve
+* @param endinggDate = ending date of the modified object
+* @return void
+*/
 	public  void modifSessionClose(Date endingDate)
 	{
 		setClose(endingDate);
 	}
-
+	
+/**
+* @brief Alter session id List(...) with a new id List(...) date
+*
+* @author Mathieu Le Veve
+* @param newlist = new list of id of the modified object
+* @return void
+*/
 	public void modifylist(int[] newlist)
 	{
 		session.setList(newlist);
 	}
 
+/**
+* @brief Alter session password with a new password
+*
+* @author Mathieu Le Veve
+* @param password = password of the modified object
+* @return void
+*/
 	public void modifSessionPassword(String password)
 	{
 		setPassword(password);
 	}
 
+/**
+* @brief Alter session type with a new type of session
+*
+* @author Mathieu Le Veve
+* @param type = type of the modified object
+* @return void
+*/
 	public void modifSessionType(boolean type)
 	{
 		setType(type);
 	}
 
+/**
+* @brief Display Info about the target session by ID
+*
+* @author Mathieu Le Veve
+* @param idsession = Session ID 
+* @return Session object
+*/
 	public Session displayInfoSession(int idsession)
 	{
 		session = new Session();
 		session = getSessionById(idsession);
-
+		//@getSessionById(int id)
+		// Alexandra Moshina's function wich return a session by id
 		return session;
+		// A cet instant, IG affichera chaque element des sessions trouvées par l'id
 	}
-
+	
+/**
+* @brief delete by ID
+*
+* @author Demogue Bruno
+* @param idsession = Session ID 
+* @return void
+*/
 	public void delete(idsession)
 	{
 		deleteSession(idsession);
+		//@deleteSession(int id)
+		// Alexandra Moshina's function wich return a session by id
 	}
 
-
-
+/**
+* @brief modify a Session adding List with an id List
+*
+* @author Demogue Bruno
+* @param idsession = Session ID 
+* @return boolean
+*/
 	public boolean modifSessionAddlist(int idl)
 	{
 		int i;
@@ -111,12 +188,17 @@ public class ActionSession
 		}
 
 		temp.list[i]=idl;
-		// Pourquoi ne pas set la session + submit pour que la BDD soit mise à jour ?
 		return true;
 	}
 
 
-
+/**
+* @brief modify a Session deleting List with an id List
+*
+* @author Demogue Bruno
+* @param idsession = Session ID 
+* @return void
+*/
 	public void modifSessionDeleteList(int idl)
 	{
 		
@@ -128,12 +210,18 @@ public class ActionSession
 			{
 				j=i;
 			}
-		/* Pb de decrementation là ??? */
 		}
 		temp.list[j]=temp.list[i-1];
 		temp.list[i-1]=null;
 	}
 
+/**
+* @brief Find session by string name
+*
+* @author LE VEVE Mathieu
+* @param name = Session name to find
+* @return boolean
+*/
 	public boolean findSession(String name)
 	{
 		int tabid[];
@@ -155,6 +243,7 @@ public class ActionSession
 			{
 				id = tabid[i];
 				displayInfoSession(id);
+				// Affiche les infos de la session
 			}
 			return true;
 		}
